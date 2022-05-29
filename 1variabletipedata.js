36a59acda698835e23be2e23b357b42391d7c986
Santi Purwaningsih <74691333+santipn@users.noreply.github.com>
2022-05-29 23:24:25 +0700

    Create 1variabletipedata.js

---
+++ 1variabletipedata.js
@@ -0,0 +1,18 @@
+const myBiodata = {
+    name: 'Santi',
+    age: 24,
+    hobbies: ['Reading', 'Learning', 'Playing game'],
+    isMarried: false,
+    schoolList: [
+        {name: 'TK Putra III', yearIn: '2003', yearOut: '2004'},
+        {name: 'MI Jelat', yearIn: '2004', yearOut: '2010'},
+        {name: 'MTs Negeri 1 Banjar', yearIn: '2010', yearOut: '2013'},
+        {name: 'SMK Negeri 2 Banjar', yearIn: '2013', yearOut: '2016', major: 'Teknik Komputer dan Jaringan'},
+    ],
+    skills: {
+        skillName: 'Javascript',
+        level: 'beginner',
+    },
+    interestInCoding: true,
+}
+console.log(myBiodata);
